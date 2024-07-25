1be3d79aa861ba43b16d5c1dda8190e666d1b8b3
Ororura <hitmanlol890@gmail.com>
2024-07-25 11:56:07 +0300

    add getStarted

---
+++ src/widgets/getStarted/ui/GetStartedWidget.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { FC, FormEventHandler, useCallback } from "react";
+import s from "./GetStartedWidget.module.scss";
+
+const GetStartedWidget: FC = () => {
+  const handler = useCallback<FormEventHandler>((e) => {
+    e.preventDefault();
+  }, []);
+
+  return (
+    <div className={s.container}>
+      <div>
+        <p className={s.getStartText}>Let’s get started</p>
+        <p className={s.desc}>Now that you know a lot about me, let me know if you are interested to work with me.</p>
+      </div>
+      <form onSubmit={handler} className={s.formInput}>
+        <p>Name</p>
+        <input type="text" />
+        <p>Email</p>
+        <input type="text" />
+        <p>Message</p>
+        <textarea id={s.messageInput} />
+        <button type="submit">Let’s get started</button>
+      </form>
+    </div>
+  );
+};
+
+export { GetStartedWidget };
---
+++ src/widgets/getStarted/ui/index.ts
@@ -0,0 +1,1 @@
+export { GetStartedWidget } from "./GetStartedWidget";
